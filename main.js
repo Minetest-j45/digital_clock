8b702575adf8c643e467f161ceb54f9947962786
HimbeerserverDE <himbeerserverde@gmail.com>
2021-10-30 17:28:43 +0200

    Fix whitespaces

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
 function getHour() {
-    var d = new Date();
-    //return d.getHours();
-    var hour = ""+d.getHours();
-    if (hour.length == 1) {
-        hour = "0" + hour;
-    }
-    return hour;
+	var d = new Date();
+	// return d.getHours();
+	var hour = ""+d.getHours();
+	if (hour.length == 1) {
+		hour = "0" + hour;
+	}
+
+	return hour;
 }
 
 function getMinute() {
-    var d = new Date();
-    //return d.getMinutes();
-    var minute = ""+d.getMinutes();
-    if (minute.length == 1) {
-        minute = "0" + minute;
-    }
-    return minute;
+	var d = new Date();
+	//return d.getMinutes();
+	var minute = ""+d.getMinutes();
+	if (minute.length == 1) {
+		minute = "0" + minute;
+	}
+
+	return minute;
 }
 
 function getSecond() {
-    var d = new Date();
-    //return d.getSeconds();
-    var second = ""+d.getSeconds();
-    if (second.length == 1) {
-        second = "0" + second;
-    }
-    return second;
+	var d = new Date();
+	//return d.getSeconds();
+	var second = ""+d.getSeconds();
+	if (second.length == 1) {
+		second = "0" + second;
+	}
+
+	return second;
 }
 
 function drawClock() {
-    var clock = document.getElementById('clock');
-    clock.innerHTML = getHour() + ":" + getMinute() + ":" + getSecond();
-    updateClock();
+	var clock = document.getElementById('clock');
+	clock.innerHTML = getHour() + ":" + getMinute() + ":" + getSecond();
+	updateClock();
 }
 
 function updateClock() {
-    setTimeout(drawClock, 1000);
+	setTimeout(drawClock, 1000);
 }
 
 drawClock();
